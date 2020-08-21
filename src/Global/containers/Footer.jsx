9f6cc0ba647111e48a11730bc3a4eb8184e73c70
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:15:58 -0400

    Created Footer component

---
+++ src/Global/containers/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className="copyright">
+                &copy; WhatYouCodingBoutWillis
+            </div>
+            <div className="misc">
+                whatyoucodingboutwillis@gmail.com
+            </div>
+        </>
+    )
+}
+
+export default Footer
