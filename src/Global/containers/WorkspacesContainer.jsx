34b5eb10b53dfc815cccbcf32e03640e7c5e3cc4
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:16:50 -0400

    added WorkspaceContainer component

---
+++ src/Global/containers/WorkspacesContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import WorkspaceNavbar from '../components/WorkspaceNavbar'
+
+
+const WorkspacesContainer = () => {
+    return (
+        <>
+            <div className="inner-nav w-full">
+                <WorkspaceNavbar />
+            </div>
+        </>
+    )
+}
+
+export default WorkspacesContainer
