126e9869bef51aed5a6ee60641585c22bcd5cc2d
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:15:21 -0400

    added styling to mock window buttons

--- src/Global/components/MockWindowButtons.jsx
+++ src/Global/components/MockWindowButtons.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const MockWindowButtons = () => {
     return (
-        <ul className="mock-btns">
-            <li className="red">1</li>
-            <li className="yellow">2</li>
-            <li className="green">3</li>
+        <ul className="mock-btns flex">
+            <li className="red h-3 w-3 rounded-full bg-red-500"></li>
+            <li className="yellow h-3 w-3 rounded-full ml-2 bg-yellow-500"></li>
+            <li className="green h-3 w-3 rounded-full ml-2 bg-green-400"></li>
         </ul>
     )
 }
