753f4b9ad9fc67cdb6c2ed0f909d80cb4fd296d9
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:16:08 -0400

    Added logo to header

--- src/Global/containers/Header.jsx
+++ src/Global/containers/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import MockWindowButtons from '../components/MockWindowButtons'
+import Logo from '../components/Logo'
 
 const Header = () => {
     return (
         <>
-            <MockWindowButtons />
-            {/* Logo */}
+            <div className="mock-window-btns">
+                <MockWindowButtons />
+            </div>
+            <div className="logo m-auto text-white text-xs">
+                <Logo />
+            </div>
         </>
     )
 }
