100d09f63893d72ad847e3e427f43f5e147b8e17
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:15:38 -0400

    Created WorkspaceNavbar component

---
+++ src/Global/components/WorkspaceNavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WorkspaceNavbar = () => {
+    return (
+        <>
+            <ul className="workspace-nav-items border border-solid border-green-500 flex">
+                <li className="about">READ.me</li>
+                <li className="projects">PROJECTS.mine</li>
+            </ul>
+        </>
+    )
+}
+
+export default WorkspaceNavbar
