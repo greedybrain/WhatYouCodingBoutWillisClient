4f21c7a0a13f2085f296f7140f6d88b0edb167ad
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:14:52 -0400

    Created InnerWorkspaces component

---
+++ src/Global/components/InnerWorkspaces.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InnerWorkspaces = () => {
+    return (
+        <>
+            {/* AboutMeWorkspace */}
+            {/* TechSkillsWorkspace */}
+        </>
+    )
+}
+
+export default InnerWorkspaces
