b01d9d6db76999296c5a982a792058613add664a
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-20 23:32:37 -0400

    added data.js file to hold specific site data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const me = {
+    name: "Naya Willis",
+    brief: {
+        bio: {
+            paragraph1: "Hello, my name is Naya Willis. I'm a very simple man. If I see, or write, code, I get excited and indulge even further (especially when it works 🤯).",
+            paragraph2: "I have a very strong curiosity and aptitude to learn new things. No matter how challenging it may seem; hours, days, weeks, or even months will have to be put into whatever it is that I'm learning until I fully comprehend it.",
+            paragraph3: "Shameless plug: I can solve the Rubik's Cube in less than 2 minutes. Emphasis on me loving to challenge myself!"
+        },
+        education: {
+            schoolName: "Flatiron School",
+            achievements: "Software Engineering, Certificate of Completion",
+            duration: "September 9, 2019 - July 27, 2020"
+        }
+    },
+    techSkills: {
+        title: "Tech Skills",
+        types: ["Programming Languages", "Libraries/Frameworks", "Databases", "Deployment"],
+        languages: ["Ruby", "Javascript", "Python"],
+        libFrameworks: ["Sinatra", "Rails", "React", "React Native", "Redux", "Node"],
+        databases: ["SQL", "Postgres", "MongoDB"],
+        deployment: ["Heroku", "Github Pages", "Netlify"]
+    }
+}
