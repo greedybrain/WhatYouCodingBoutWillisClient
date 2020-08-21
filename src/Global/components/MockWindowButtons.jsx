5807e7119a0385caadf28f9dad7e5d9361988f77
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 10:41:31 -0400

    Created MockWindowButtons component

---
+++ src/Global/components/MockWindowButtons.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MockWindowButtons = () => {
+    return (
+        <ul className="mock-btns">
+            <li className="red">1</li>
+            <li className="yellow">2</li>
+            <li className="green">3</li>
+        </ul>
+    )
+}
+
+export default MockWindowButtons
