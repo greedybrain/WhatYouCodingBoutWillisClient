ec00eb122be92caa104ec915035f74ebb7051fa1
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 10:14:31 -0400

    added EditorContainer to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import EditorContainer from './Global/EditorContainer';
 
 function App() {
   return (
     <div className="App">
+      <EditorContainer />
     </div>
   );
 }
