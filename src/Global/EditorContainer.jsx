2e4d6882fe232e282ea61d9c47e679641f8a3f34
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 10:15:34 -0400

    Created EditorContainer component

---
+++ src/Global/EditorContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditorContainer = () => {
+    return (
+        <div className="editor-container">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default EditorContainer
