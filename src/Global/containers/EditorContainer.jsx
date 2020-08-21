92ddd430167ec1795a7a396d9fc435f7ced85cbd
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 10:41:12 -0400

    file relovated

---
+++ src/Global/containers/EditorContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const EditorContainer = () => {
+    return (
+        <div className="editor-container border-2 border-solid border-black">
+            <header>
+                <Header />
+            </header>
+        </div>
+    )
+}
+
+export default EditorContainer
