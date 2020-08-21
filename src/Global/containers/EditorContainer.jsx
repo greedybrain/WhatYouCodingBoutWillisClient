ec7dbc1f3c8c074e09162b69fee50d97c6c8ed32
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:16:22 -0400

    Added footer and inner editor

--- src/Global/containers/EditorContainer.jsx
+++ src/Global/containers/EditorContainer.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Header from './Header'
+import InnerEditorContainer from './InnerEditorContainer'
+import Footer from './Footer'
+
 
 const EditorContainer = () => {
     return (
-        <div className="editor-container border-2 border-solid border-black">
-            <header>
+        <div className="editor-container h-screen font-mono">
+            <header className="header flex justify-between pl-2 items-center bg-gray-700">
                 <Header />
             </header>
+            <section className="inner-editor-container border border-solid border-black flex w-full">
+                <InnerEditorContainer />
+            </section>
+            <footer className="flex justify-between px-2">
+                <Footer />
+            </footer>
         </div>
     )
 }
