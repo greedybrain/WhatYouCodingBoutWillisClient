df9e10dbd02dccab6b78b14225e6cba731d680f5
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-21 13:14:39 -0400

    Created Explorer Navbar component

---
+++ src/Global/components/ExplorerNavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ExplorerNavbar = () => {
+    return (
+        <>
+            <ul className="explorer-nav-items">
+                <li className="about"><i className="fas fa-user-astronaut"></i></li>
+                <li className="projects"><i className="fab fa-github-alt"></i></li>
+            </ul>
+        </>
+    )
+}
+
+export default ExplorerNavbar
